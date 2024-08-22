6d7075132f378ffe344f26eec3b8b3a899bfe3fe
kumar4532 <krantikumar2001@gmail.com>
2024-08-22 18:37:20 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,10 @@ app.use("/api/auth", authRoutes)
 app.use("/api/posts", postRoutes)
 app.use("/api/comment", commentRoutes)
 
-app.use(express.static(path.join(__dirname, 'dist/')));
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/', 'index.html'));
+  res.sendFile(path.join(__dirname, "frontend",  'dist', 'index.html'));
 });
 
 connectDB()
