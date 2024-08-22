79b8a9104cab64e8b013223cefb05e5a6a81b4eb
kumar4532 <krantikumar2001@gmail.com>
2024-08-22 18:24:51 +0530

    change in vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist/frontend',
+  },
   server: {
     proxy: {
-			"/api": {
-				target: "http://localhost:5173",
-			},
-		},
-  }
-})
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
