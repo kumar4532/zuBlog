1fca079885e66c2c8c041783baca266ea97e4a54
kumar4532 <krantikumar2001@gmail.com>
2024-08-22 18:09:42 +0530

    add imports

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import connectDB from "./DB/connect.js"
 import cookieParser from "cookie-parser";
 import cors from "cors"
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js"
 import postRoutes from "./routes/post.routes.js"
